9a6704b6d0cbe26f6986bbfa8bd95043369f4866
Nikita <superduperproger@gmail.com>
2020-01-14 19:21:13 +0300

    Fixed error with class name v0.1.4

--- application/core/View.php
+++ application/core/View.php
@@ -9,7 +9,7 @@ class View
 
         if ( ! file_exists( $require_path ) )
         {
-            DefaultModel::openAndWriteFileLogs("\nFile with error code: $error_code, not found on this server! \n path: $require_path\n");
+            Model::openAndWriteFileLogs("\nFile with error code: $error_code, not found on this server! \n path: $require_path\n");
             return false;
         } else {
             require_once ( $require_path );
@@ -22,7 +22,7 @@ class View
         if ( ! file_exists ( ROOT . '/public/temp/' . $temp_name . '.html' ) )
         {
             //Пишем в лог файл
-            DefaultModel::openAndWriteFileLogs("File (template) with name: $temp_name, not found on this server!");
+           Model::openAndWriteFileLogs("File (template) with name: $temp_name, not found on this server!");
             return false;          
         } else {
             require_once ( ROOT . '/public/temp/' . $temp_name . '.html');
