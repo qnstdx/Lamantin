9d44dcfc02b6328d28229034ffc5bc043717721e
Nikita <superduperproger@gmail.com>
2020-01-15 15:02:36 +0300

    add database sql dump v0.1.4

---
+++ bd_damp.sql
@@ -0,0 +1,21 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `foo` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `foo` (`id`, `name`, `text`) VALUES
+(1, 'Vasya', '123');
+
+ALTER TABLE `foo`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `foo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
