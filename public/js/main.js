206652cee68fa15184e68cbb015f106b8d3ffebe
Nikita <superduperproger@gmail.com>
2020-01-19 09:37:37 +0300

    Added VueJS v0.2.3

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
-alert("hello");
+/*
+* |-------------------------------------|
+* |                                     |
+* |            Vue JS code              |
+* |   powered by https://vuejs.org   |
+* |                                     |
+* |-------------------------------------|
+* */
+let app = new Vue({
+    el: '#app',
+    // Чтобы не было конфликта с Twig
+    delimiters: ['${', '}'],
+    data: {
+        vue_answer: 'VueJS Worked!',
+        data_last_update: 'Last update: 18.01.2020',
+        seen: true
+    },
+    methods: {
+        opacityGrappy: function () {
+            this.seen = false
+        }
+    }
+});
